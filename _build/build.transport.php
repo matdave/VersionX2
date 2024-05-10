e3f0b320cb0ac99f9f2f5167977b3084b050e32c
Murray Wood <murray@digitalpenguin.studio>
2024-05-10 14:51:44 +0800

    Prep for release

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -11,7 +11,7 @@ if (!defined('MOREPROVIDER_BUILD')) {
     /* define version */
     define('PKG_NAME', 'VersionX');
     define('PKG_NAMESPACE', 'versionx');
-    define('PKG_VERSION', '3.0.1');
+    define('PKG_VERSION', '3.1.0');
     define('PKG_RELEASE', 'pl');
 
     /* load modx */
@@ -26,12 +26,11 @@ if (!defined('MOREPROVIDER_BUILD')) {
     echo '<pre>';
     flush();
     $targetDirectory = dirname(dirname(__FILE__)) . '/_packages/';
-}
-else {
+} else {
     $targetDirectory = MOREPROVIDER_BUILD_TARGET;
 }
 
-$root = dirname(dirname(__FILE__)).'/';
+$root = dirname(dirname(__FILE__)) . '/';
 $sources = [
     'root' => $root,
     'build' => $root . '_build/',
@@ -49,7 +48,7 @@ $sources = [
 ];
 unset($root);
 
-$modx= new modX();
+$modx = new modX();
 $modx->initialize('mgr');
 $modx->setLogLevel(modX::LOG_LEVEL_INFO);
 $modx->setLogTarget('ECHO');
@@ -93,40 +92,40 @@ $builder->package->put(
         'vehicle_class' => 'xPDOFileVehicle',
     ],
 );
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in files.');
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in files.');
 flush();
 
 // Category
-$category= $modx->newObject('modCategory');
+$category = $modx->newObject('modCategory');
 $category->set('id', 1);
 $category->set('category', PKG_NAME);
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in category.');
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in category.');
 flush();
 
 // Settings
-$settings = include_once $sources['data'].'transport.settings.php';
-$attributes= [
+$settings = include_once $sources['data'] . 'transport.settings.php';
+$attributes = [
     xPDOTransport::UNIQUE_KEY => 'key',
     xPDOTransport::PRESERVE_KEYS => true,
     xPDOTransport::UPDATE_OBJECT => false,
 ];
 if (!is_array($settings)) {
-    $modx->log(modX::LOG_LEVEL_FATAL,'Adding settings failed.');
+    $modx->log(modX::LOG_LEVEL_FATAL, 'Adding settings failed.');
 }
 foreach ($settings as $setting) {
-    $vehicle = $builder->createVehicle($setting,$attributes);
+    $vehicle = $builder->createVehicle($setting, $attributes);
     $builder->putVehicle($vehicle);
 }
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($settings).' system settings.');
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in ' . count($settings) . ' system settings.');
 flush();
-unset($settings,$setting,$attributes);
+unset($settings, $setting, $attributes);
 
 // Plugins
 $plugins = include $sources['data'] . 'transport.plugins.php';
 if (!is_array($plugins)) {
-    $modx->log(modX::LOG_LEVEL_FATAL,'Adding plugins failed.');
+    $modx->log(modX::LOG_LEVEL_FATAL, 'Adding plugins failed.');
 }
-$attributes= [
+$attributes = [
     xPDOTransport::UNIQUE_KEY => 'name',
     xPDOTransport::PRESERVE_KEYS => false,
     xPDOTransport::UPDATE_OBJECT => true,
@@ -143,13 +142,31 @@ foreach ($plugins as $plugin) {
     $vehicle = $builder->createVehicle($plugin, $attributes);
     $builder->putVehicle($vehicle);
 }
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in ' . count($plugins) . ' plugins.');
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in ' . count($plugins) . ' plugins.');
 flush();
 unset($plugins, $plugin, $attributes);
 
 // Menus
-require_once ($sources['data'] . 'transport.menus.php');
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in menus');
+require_once($sources['data'] . 'transport.menus.php');
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in menus');
+
+// Dashboard Widgets
+$widgets = include $sources['data'] . 'transport.dashboardwidgets.php';
+if (empty($widgets)) {
+    $modx->log(modX::LOG_LEVEL_ERROR, 'Could not package in widgets.');
+}
+$attributes = [
+    xPDOTransport::PRESERVE_KEYS => false,
+    xPDOTransport::UPDATE_OBJECT => true,
+    xPDOTransport::UNIQUE_KEY => ['name'],
+];
+foreach ($widgets as $widget) {
+    $vehicle = $builder->createVehicle($widget, $attributes);
+    $builder->putVehicle($vehicle);
+}
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in ' . count($widgets) . ' widgets.');
+flush();
+unset($widgets, $widget, $attributes);
 
 // Category Vehicle
 $attr = [
@@ -168,14 +185,8 @@ $vehicle->resolve('php', [
 $vehicle->resolve('php', [
     'source' => $sources['resolvers'] . 'setupoptions.resolver.php',
 ]);
-//$vehicle->resolve('php', [
-//    'source' => $sources['resolvers'] . 'dashboardwidgets.resolver.php',
-//]);
-//$vehicle->resolve('php', [
-//    'source' => $sources['resolvers'] . 'cleanup.resolver.php',
-//]);
 
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in resolvers.');
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in resolvers.');
 flush();
 $builder->putVehicle($vehicle);
 
@@ -185,13 +196,13 @@ $builder->setPackageAttributes([
     'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
     'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
     'setup-options' => [
-        'source' => $sources['build'].'setup.options.php',
+        'source' => $sources['build'] . 'setup.options.php',
     ],
 ]);
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in package attributes.');
+$modx->log(modX::LOG_LEVEL_INFO, 'Packaged in package attributes.');
 flush();
 
-$modx->log(modX::LOG_LEVEL_INFO,'Packing...');
+$modx->log(modX::LOG_LEVEL_INFO, 'Packing...');
 flush();
 $builder->pack();
 
@@ -202,7 +213,7 @@ $tend = $mtime;
 $totalTime = ($tend - $tstart);
 $totalTime = sprintf("%2.4f s", $totalTime);
 
-$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
+$modx->log(modX::LOG_LEVEL_INFO, "\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
 
 /**
  * @param string $filename
